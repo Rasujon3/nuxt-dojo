62303e5a2a633df0726041052164c05b1624ab23
Rasujon3 <rasujon3@gmail.com>
2024-12-09 22:05:03 +0600

    netninja basic tutorial done

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  modules: ['@nuxtjs/tailwindcss'],
+  compatibilityDate: '2024-12-09',
+  app: {
+    head: {
+      title: 'Nuxt Dojo',
+      meta: [
+        {name: 'description', content: 'Everything about Nuxt 3'}
+      ],
+      link: [
+        {rel: 'stylesheet', href: 'https://fonts.googleapis.com/icon?family=Material+Icons'}
+      ]
+    }
+  },
+  runtimeConfig: {
+    currencyKey: process.env.CURRENCY_API_KEY
+  }
 })
---
+++ pages/about.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <h2>About</h2>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eius illum aliquam quidem ipsam veritatis autem non numquam, dolorum vitae porro?</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eius illum aliquam quidem ipsam veritatis autem non numquam, dolorum vitae porro?</p>
+        <p>api response:</p>
+        <div>{{ data }}</div>
+    </div>
+</template>
+
+<script setup>
+  // const { data } = await useFetch('/api/ninja?name=mario')
+  // const { data } = await useFetch('/api/ninja?name=mario', {
+  //   method: 'post',
+  //   body: {age: 30}
+  // })
+
+//   const { data } = await useFetch('/api/ninja')
+  const { data } = await useFetch('/api/currency/GBP')
+</script>
+
+<style scoped>
+h2 {
+    margin-bottom: 20px;
+    font-size: 36px;
+}
+p {
+    margin: 20px 0;
+}
+
+</style>
